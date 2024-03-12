4d0948e5d4b0da2f4593555098bab7f58dd44ba4
jinrunheng <1175088275@qq.com>
2024-03-12 22:53:12 +0800

    finished ruanyf crwaler

--- automatically-publish-articles-on-wechat-ai/src/main/java/com/github/article_crawler/RyfArticleCrawler.java
+++ automatically-publish-articles-on-wechat-ai/src/main/java/com/github/article_crawler/RyfArticleCrawler.java
@@ -6,9 +6,12 @@ import org.kohsuke.github.GHRepository;
 import org.kohsuke.github.GitHub;
 import org.kohsuke.github.PagedIterable;
 
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.net.URL;
 import java.util.List;
+import java.util.Properties;
 
 /**
  * @Author Dooby Kim
@@ -18,10 +21,30 @@ import java.util.List;
  */
 @Slf4j
 public class RyfArticleCrawler {
+    private static String login;
+    private static String token;
+    private static String keyPath = "src/test/resources/github.properties";
+
+    private static void initKey() {
+        Properties prop = new Properties();
+
+        try (FileInputStream input = new FileInputStream(keyPath)) {
+            // 从输入流加载属性列表
+            prop.load(input);
+            // 获取属性
+            login = prop.getProperty("login");
+            token = prop.getProperty("token");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
     private static final String repoName = "ruanyf/weekly";
 
     public static void crawler(String repo) throws IOException {
-        GitHub gitHub = GitHub.connectAnonymously();
+        initKey();
+        final GitHub gitHub = GitHub.connect(login, token);
+
         GHRepository repository = gitHub.getRepository(repo);
         final PagedIterable<GHCommit> commits = repository.listCommits();
         final List<GHCommit> ghCommits = commits.asList();
@@ -44,8 +67,26 @@ public class RyfArticleCrawler {
         }
         log.info("recentArticleCommit", recentArticleCommit);
         recentArticleCommit.getFiles().forEach(file -> {
-            final URL blobUrl = file.getBlobUrl();
-            log.info("blobUrl", blobUrl);
+            if (file.getFileName().startsWith("docs/issue")) {
+                final String patch = file.getPatch();
+                // 去除掉所有的 "+" 字符
+                final String originalText = patch.replace("+", "").trim();
+                StringBuilder oneLineBuilder = new StringBuilder();
+                for (char c : originalText.toCharArray()) {
+                    if (c != '\n') { // 如果不是换行符，则追加到StringBuilder中
+                        oneLineBuilder.append(c);
+                    }
+                }
+
+                String oneLineString = oneLineBuilder.toString();
+                String fileName = "output.md";
+                try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+                    writer.write(oneLineString);
+                    log.info("Markdown text has been written to " + fileName);
+                } catch (IOException e) {
+                    log.error("error", e);
+                }
+            }
         });
     }
 
